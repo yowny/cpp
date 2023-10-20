f6c9b56ba5ab11fc34fd05f36bacdab24356c902
Doyle Wang <wtyforseek@gmail.com>
2023-10-20 14:14:22 +0800

    update src

--- src/practice.cpp
+++ src/practice.cpp
@@ -4,14 +4,44 @@
 #include <vector>
 #include <array>
 #include <complex>
+#include <algorithm>
 using namespace std;
+class Rational
+{
+public:
+    Rational(int numerator = 0, int denominator = 1) : num(numerator), den(denominator) {}
+    int nmerator() const
+    {
+        return num;
+    }
+    int denominator() const
+    {
+        return den;
+    }
+
+private:
+    int num;
+    int den;
+};
+
+const Rational operator*(const Rational &lhs,const Rational &rhs)
+{
+    int newden = lhs.denominator() * rhs.denominator();
+    int newnum = lhs.nmerator()*rhs.nmerator();
+    int gcd = std::__gcd(newnum, newden);
+    Rational newRational(newnum / gcd, newden / gcd);
+    return newRational;
+}
+
+
 int main(int argc, char const *argv[])
 {
-    int *pi;//指针需要判空
-    int ival = 1024;
-    pi = &ival;
-    cout << pi << endl;//地址
-    cout << *pi << endl;
-
+    Rational oneEighth(1, 8);
+    Rational oneHalf(1, 2);
+    //Rational result = oneHalf * oneEighth;
+    //Rational result = oneHalf * 2;
+    Rational result = 2* oneHalf;
+    cout << "num = " << result.nmerator() << endl;
+    cout << "den = " << result.denominator() << endl;
     return 0;
 }
--- src/tempCodeRunnerFile.cpp
+++ src/tempCodeRunnerFile.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include <cstring>
-#include <string>
-#include <vector>
-#include <array>
-#include <complex>
-using namespace std;
-int main(int argc, char const *argv[])
-{
-    int *pi;
-    int ival = 1024;
-    pi = &ival;
-    cout << pi << endl;
-    cout << *pi << endl;
-    return 0;
-}
+n
